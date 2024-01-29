753e5ee25d8576c372f8c3fc18391a77b489afd9
c huseby <catherine_huseby@outlook.com>
2024-01-29 08:05:36 -0600

    yay

---
+++ ViewControllerFavorites.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerFavorites.swift
+//  mySuperEpicMovieAppHuseby
+//
+//  Created by CATHERINE HUSEBY on 1/24/24.
+//
+
+import UIKit
+
+class ViewControllerFavorites: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
